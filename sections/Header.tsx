173704b336603cf61cd14db5c6c335fc3a756659
Chance <chance@nei.house>
2024-07-25 07:19:39 -0700

    chore: Update header component styling

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,30 +1,28 @@
-
-/** @jsx h */
 import { h } from "preact";
 
 export default function Header() {
   return (
-    <header class="bg-gray-800 text-white">
-      <div class="container mx-auto flex justify-between items-center p-4">
-        <a href="/" class="logo">
-          <img src="/logo.svg" alt="Primordial Groove Logo" class="h-8" />
+    <header className="bg-gray-800 text-white">
+      <div className="container mx-auto flex justify-between items-center p-4">
+        <a href="/" className="logo">
+          <img src="/logo.svg" alt="Primordial Groove Logo" className="h-8" />
         </a>
-        <nav class="hidden md:flex">
-          <ul class="flex space-x-4">
-            <li><a href="#" class="hover:text-gray-400">About</a></li>
-            <li><a href="#" class="hover:text-gray-400">Events</a></li>
-            <li><a href="#" class="hover:text-gray-400">Join Us</a></li>
+        <nav className="hidden md:flex">
+          <ul className="flex space-x-4">
+            <li><a href="#" className="hover:text-gray-400">About</a></li>
+            <li><a href="#" className="hover:text-gray-400">Events</a></li>
+            <li><a href="#" className="hover:text-gray-400">Join Us</a></li>
           </ul>
         </nav>
         <div>
           <p>Hello from decopilot</p>
         </div>
       </div>
-      <nav id="mobile-menu" class="hidden md:hidden">
-        <ul class="flex flex-col space-y-4 p-4">
-          <li><a href="#" class="hover:text-gray-400">About</a></li>
-          <li><a href="#" class="hover:text-gray-400">Events</a></li>
-          <li><a href="#" class="hover:text-gray-400">Join Us</a></li>
+      <nav id="mobile-menu" className="hidden md:hidden">
+        <ul className="flex flex-col space-y-4 p-4">
+          <li><a href="#" className="hover:text-gray-400">About</a></li>
+          <li><a href="#" className="hover:text-gray-400">Events</a></li>
+          <li><a href="#" className="hover:text-gray-400">Join Us</a></li>
         </ul>
       </nav>
     </header>
